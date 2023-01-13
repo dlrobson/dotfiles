6f760c1ed60d8313410f76bd0dd3409a96a3aef9
Dan Robson <REDACTED>
2023-01-13 07:26:51 -0500

    setup script and tmux added

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+# This script sets up symlinks to all the dotfiles
+# in the user's home directory.
+
+declare base=${HOME}/dotfiles
+
+# Check for required dependencies before continuing:
+if [[ ! -a $(which git) ]]; then
+  echo "Error: git is not installed. Please install git first."
+  exit 1
+fi
+
+if [[ ! -a $(which curl) ]]; then
+  echo "Error: curl is not installed. Please install curl first."
+  exit 1
+fi
+
+if [[ ! -a $(which stow) ]]; then
+  echo "Error: stow is not installed. Please install stow first."
+  exit 1
+fi
+
+if [[ ! -a $(which unzip) ]]; then
+  echo "Error: unzip is not installed. Please install unzip first."
+  exit 1
+fi
+
+# Set up tmux plugin manager:
+mkdir -p ~/.tmux/plugins
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Set up all of the configs:
+cd ${base}/stow
+
+# This for loop iterates through all directories
+# contained in the stow directory. This makes
+# it easy to add configurations for new applications
+# without having to modify this script.
+for app in */; do
+	stow -t ${HOME} $app
+done;
