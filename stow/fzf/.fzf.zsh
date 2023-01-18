e92f530ada4707ecc2c3e6d901b3c3a4417285df
Dan Robson <daniel.robson@ouster.io>
2023-01-18 08:35:35 -0500

    README and neovim added

--- stow/fzf/.fzf.zsh
+++ stow/fzf/.fzf.zsh
@@ -1,2 +1,13 @@
-source /usr/share/doc/fzf/examples/key-bindings.zsh
-source /usr/share/doc/fzf/examples/completion.zsh
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/daniel.robson/.local/share/nvim/site/pack/packer/start/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/daniel.robson/.local/share/nvim/site/pack/packer/start/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/daniel.robson/.local/share/nvim/site/pack/packer/start/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/daniel.robson/.local/share/nvim/site/pack/packer/start/fzf/shell/key-bindings.zsh"
