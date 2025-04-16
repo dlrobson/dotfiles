2e707246321119852c6bb19eb7392ca575e3d878
Daniel Robson <danr.236@gmail.com>
2025-04-16 19:58:25 -0400

    chore: add tmux support

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -68,4 +68,30 @@
       set backspace=2
     '';
   };
+
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    historyLimit = 100000;
+    terminal = "screen-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = yank;
+        extraConfig = "set -g @yank_selection_mouse 'clipboard'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = "set -g @continuum-restore 'on'";
+      }
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-capture-pane-contents 'on'";
+      }
+      {
+        plugin = gruvbox;
+        extraConfig = "set -g @tmux-gruvbox 'dark'";
+      }
+    ];
+  };
 }
