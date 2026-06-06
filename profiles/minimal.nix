adc176eaed76d12dad0fc9093a6158af31453617
Daniel Robson <danr.236@gmail.com>
2026-06-05 21:22:29 -0400

    feat: add profiles/minimal.nix and profiles/desktop.nix entry points

---
+++ profiles/minimal.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  homeDirectory = builtins.getEnv "HOME";
+  username = builtins.getEnv "USER";
+in
+{
+  imports = [ ../home ];
+
+  home-manager-configuration = {
+    enable = true;
+    profile = "minimal";
+    inherit username homeDirectory;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
