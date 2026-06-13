52d60430e3825d4fe04175b2c613074eda5fdad5
Daniel Robson <danr.236@gmail.com>
2026-06-13 18:59:20 -0400

    fix: ordering of tmux plugins

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -16,12 +16,12 @@
         extraConfig = "set -g @yank_selection_mouse 'clipboard'";
       }
       {
-        plugin = continuum;
-        extraConfig = "set -g @continuum-restore 'on'";
-      }
-      {
         plugin = resurrect;
         extraConfig = "set -g @resurrect-capture-pane-contents 'on'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = "set -g @continuum-restore 'on'";
       }
       {
         plugin = catppuccin;
